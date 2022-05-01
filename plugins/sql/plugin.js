15817b3540295b6e9852413207ae4e35592f6d3f
DarkWinzo <93755320+DarkWinzo@users.noreply.github.com>
2022-05-01 16:24:14 +0530

    Add files via upload

---
+++ plugins/sql/plugin.js
@@ -0,0 +1,42 @@
+/*
+ ██████╗ ██╗   ██╗███████╗███████╗███╗   ██╗    ██████╗  ██████╗ ████████╗
+██╔═══██╗██║   ██║██╔════╝██╔════╝████╗  ██║    ██╔══██╗██╔═══██╗╚══██╔══╝
+██║   ██║██║   ██║█████╗  █████╗  ██╔██╗ ██║    ██████╔╝██║   ██║   ██║   
+██║▄▄ ██║██║   ██║██╔══╝  ██╔══╝  ██║╚██╗██║    ██╔══██╗██║   ██║   ██║   
+╚██████╔╝╚██████╔╝███████╗███████╗██║ ╚████║    ██████╔╝╚██████╔╝   ██║   
+ ╚══▀▀═╝  ╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═══╝    ╚═════╝  ╚═════╝    ╚═╝   
+Codded by @DarkWinzo                                                             
+Telegram: https://t.me/DarkWinzo                                                        
+Whatsapp: https://Wa.me/+94705193038
+Instagram:https://www.instagram.com/darkwinzo
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+WhatsApp bot - Isuru Lakshan
+*/
+
+const config = require('../../config');
+const { DataTypes } = require('sequelize');
+
+const PluginDB = config.DATABASE.define('Plugin', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    url: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    }
+});
+
+async function installPlugin(adres, file) {
+    var Plugin = await PluginDB.findAll({
+        where: {url: adres}
+    });
+
+    if (Plugin.length >= 1) {
+        return false;
+    } else {
+        return await PluginDB.create({ url: adres, name: file });
+    }
+}
+module.exports = { PluginDB: PluginDB, installPlugin: installPlugin };
